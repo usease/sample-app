381389165523da35fb9ed25ca37381161e184389
Work <azizjon_kholmatov@epam.com>
2024-03-18 08:01:10 +0500

    adds implementation for exhibit details

--- app/src/main/java/com/example/sampleapp/ui/exhibit_details/ExhibitDetailsFragment.kt
+++ app/src/main/java/com/example/sampleapp/ui/exhibit_details/ExhibitDetailsFragment.kt
@@ -1,6 +1,82 @@
 package com.example.sampleapp.ui.exhibit_details
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.sampleapp.R
+import com.example.sampleapp.base.BaseFragment
+import com.example.sampleapp.databinding.FragmentExhibitDetailsBinding
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ExhibitDetailsFragment: Fragment(R.layout.fragment_exhibit_details)
+class ExhibitDetailsFragment: BaseFragment(R.layout.fragment_exhibit_details) {
+
+    override val TAG: String = ExhibitDetailsFragment::class.simpleName!!
+
+    private lateinit var binding: FragmentExhibitDetailsBinding
+    private val viewModel: ExhibitDetailsViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentExhibitDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val objectNumber = "BK-AM-33-J"
+        viewModel.getExhibitDetails(objectNumber)
+        collectUiState()
+    }
+
+    private fun collectUiState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    if(it.isLoading) {
+                        binding.progressBar.visibility = View.VISIBLE
+                        binding.tvMessage.visibility = View.INVISIBLE
+                        binding.gContent.visibility = View.INVISIBLE
+                    } else {
+                        binding.progressBar.visibility = View.INVISIBLE
+
+                    }
+
+                    // Events
+                    it.showMessage?.getContentIfNotHandled()?.let { message ->
+                        showAndLogD(message)
+                        binding.tvMessage.text = message
+                        binding.tvMessage.visibility = View.VISIBLE
+                    }
+
+                    it.showErrorMessage?.getContentIfNotHandled()?.let { message ->
+                        showAndLogE(message)
+                        binding.tvMessage.text = message
+                        binding.tvMessage.visibility = View.VISIBLE
+                    }
+
+                    it.exhibitDetails?.getContentIfNotHandled()?.let { exhibitDetails ->
+                        //TODO set title to action bar
+                        //actionBar.title = exhibitDetails.title
+
+                        binding.tvObjectNumber.text = exhibitDetails.objectNumber
+                        binding.tvMakerLine.text = exhibitDetails.makerLine
+                        binding.tvDesc.text = exhibitDetails.desc
+                        Picasso.get().load(exhibitDetails.imageUrl).into(binding.iv)
+                        binding.tvMaterials.text = String.format(getString(R.string.materials), exhibitDetails.materials.joinToString(", "))
+                        binding.tvTechniques.text = String.format(getString(R.string.techniques), exhibitDetails.techniques.joinToString(", "))
+
+                        binding.gContent.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/sampleapp/ui/exhibit_details/ExhibitDetailsViewModel.kt
+++ app/src/main/java/com/example/sampleapp/ui/exhibit_details/ExhibitDetailsViewModel.kt
@@ -1,4 +1,56 @@
 package com.example.sampleapp.ui.exhibit_details
 
-class ExhibitDetailsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sampleapp.network.ApiEmptyResponse
+import com.example.sampleapp.network.ApiErrorResponse
+import com.example.sampleapp.network.ApiSuccessResponse
+import com.example.sampleapp.repository.MuseumRepo
+import com.example.sampleapp.utils.Event
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class ExhibitDetailsViewModel (private val repo: MuseumRepo): ViewModel() {
+
+    private val _uiState = MutableStateFlow(ExhibitDetailsState())
+    val uiState: StateFlow<ExhibitDetailsState> = _uiState.asStateFlow()
+
+    fun getExhibitDetails(objectNumber: String) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                isLoading = true
+            )
+        }
+        viewModelScope.launch {
+            when (val response = repo.getExhibitDetails(objectNumber)) {
+                is ApiSuccessResponse -> {
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            isLoading = false,
+                            exhibitDetails = Event(response.body)
+                        )
+                    }
+                }
+                is ApiEmptyResponse -> {
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            isLoading = false,
+                            showMessage = Event( "Empty response received while loading exhibit details.")
+                        )
+                    }
+                }
+                is ApiErrorResponse -> {
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            isLoading = false,
+                            showErrorMessage = Event( "Error occurred while loading exhibit details. " + response.errorMessage)
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
